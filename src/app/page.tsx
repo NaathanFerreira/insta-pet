0ddadc19793ba6bda610c45e0d1fb5ead7da13b3
Nathan <nathanferreira00@hotmail.com>
2024-03-05 15:46:27 -0300

    feat: photos feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import photosGet from '@/actions/photos-get'
+import Feed from '@/components/feed/feed'
+
+export default async function Home() {
+  const photos = await photosGet()
+
   return (
     <section className="container mainContainer">
-      <h1>Insta Pet</h1>
+      <Feed photos={photos} />
     </section>
   )
 }
